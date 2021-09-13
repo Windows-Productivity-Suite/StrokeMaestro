9f7c38345e0b372bafd34fb2b519a2b4fa6976a9
Jinmay Jhaveri <jinmayjhaveri006@gmail.com>
2021-09-13 12:01:44 +0530

    style: improve folder structure

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ function createWindow() {
   });
 
   // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, "../index.html"));
+  mainWindow.loadFile(path.join(__dirname, "../client/index.html"));
   mainWindow.setMenu(null);
 }
 
